0ad9372c97513c3808cf17a7587197591dd95da5
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-04-03 13:45:49 +0300

    telegram bot added

--- hogwarts/main/views.py
+++ hogwarts/main/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render, redirect
 from .forms import *
+from telepot import Bot
+
+bot = Bot('Your Bot Token')
 
 def home_view(request):
     if request.method == 'POST':
         contactForm = ContactForm(request.POST)
         registerForm = RegisterForm(request.POST)
         if contactForm.is_valid():
+            name = request.POST.get('full_name')
+            email = request.POST.get('email')
+            msg = request.POST.get('message')
+            bot.sendMessage('Your Id', f'Hey New Contact\n{name}\n{email}\n{msg}')
             contactForm.save()
             return redirect('/')
         if registerForm.is_valid():
+            name = request.POST.get('full_name')
+            phone = request.POST.get('phone')
+            degree = request.POST.get('degree')
+            bot.sendMessage('1060158414', f'Hey New Student\n{name}\n{phone}\n{degree}')
             registerForm.save()
             return redirect('/')
     else:
